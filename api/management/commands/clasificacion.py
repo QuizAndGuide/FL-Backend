7cf5c59c296a5a21bc24471515ff849b1f5f6437
rbc33 <ricardobenthem@gmail.com>
2023-11-01 21:56:40 +0100

    all versions of clasification script

--- api/management/commands/clasificacion.py
+++ api/management/commands/clasificacion.py
@@ -21,7 +21,22 @@ class Command(BaseCommand):
                 else:
                     errors = serializer.errors
                     print(f"Validation errors for team: {errors}")
-
+                    
+                # serializer = Clasificacion(data)
+                
+                # serializer.save() 
+                
+                # # clasificacion_data = {
+                # #     "table": data.get("table", []),
+                # #     "info": data.get("info", {}),
+                # #     "legends": data.get("legends", []),
+                # # }
+                
+                # # # Guarda los datos en el modelo Clasificacion
+                # # clasificacion = Clasificacion(**clasificacion_data)
+                # # clasificacion.save()
+                
+                self.stdout.write("Datos de clasificación guardados correctamente.")
             # Error handling for unsuccessful requests
             else:
                 self.stderr.write(f'Error en la solicitud. Código de respuesta: {response.status_code}'if response.status_code != 200 else response.status_code)
