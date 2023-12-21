9b3063f9f62ec714902a6c4e89c9947541a11d98
rbc33 <ricardobenthem@gmail.com>
2023-12-21 18:54:34 +0100

    creates languaje and configuration and conf endpoint

--- api/urls.py
+++ api/urls.py
@@ -6,11 +6,12 @@ from .views import *
 router = routers.DefaultRouter()
 router.register('profile', ProfileViewSet)
 # router.register('jornada', views.JornadaViewSet, basename='partidos')
-# router.register('stats', views.MaxGoalersViewSet, basename='goalers')
+# router.register('stats', views.ConfigurationViewSet, basename='goalers')
 
 urlpatterns = [
     path('nextmatches/', NextMatchesView.as_view(), name='proximos partidos'),
     path('lastmatches/', LastMatchesView.as_view(), name='ultimos partidos'),
     path('goalers/', MaxGoalersView.as_view(), name='scorers'),
+    path('conf/', ConfigurationView.as_view(), name='configuration'),
 ]
 urlpatterns += router.urls
