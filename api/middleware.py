bc538b8b57047cbae753b6385e269c029407934a
rbc33 <ricardobenthem@gmail.com>
2024-01-13 22:24:17 +0100

    miggleware redirect to auth/jwt/create if token not provided

--- api/middleware.py
+++ api/middleware.py
@@ -1,4 +1,4 @@
-from models import RoundMatch 
+from .models import RoundMatch 
 from django.utils import timezone
 from api.management.commands.update import Command as update_db
 
@@ -19,3 +19,30 @@ class AutoUpdater:
             
             return response
              
+<<<<<<< Updated upstream
+=======
+# viendo si implementar la llamada del comando update como middleware o en celery, o ambos
+
+# todavia dandole vueltas a la logica para ver si esta desactualizada la base de datos
+
+from django.shortcuts import redirect
+from rest_framework.permissions import IsAuthenticated
+
+class JWTAuthMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        if (
+            response.status_code == 401
+            and "Authentication credentials were not provided." in response.data.get("detail", "")
+        ):
+            # Almacenar la URL original antes de redirigir
+            request.session['original_path'] = request.path
+            # Redirigir a auth/jwt/create si las credenciales no fueron proporcionadas
+            return redirect('/auth/jwt/create')
+
+        return response
+
+>>>>>>> Stashed changes
--- football_quiz/settings.py
+++ football_quiz/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'api.middleware.JWTAuthMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
